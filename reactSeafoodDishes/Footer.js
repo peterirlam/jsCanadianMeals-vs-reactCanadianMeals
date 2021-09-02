38ae31401ba101067a0b418a50217e97130b99d0
Peter Irlam <irlampeter@gmail.com>
2021-09-02 15:00:31 +0100

    Create Footer.js

---
+++ reactSeafoodDishes/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div style={{backgroundColor: '#1D8D84'}}>
+      <p style={{color: 'white', paddingBottom: 20}}>Website designed by IrlTech Ltd, part of the IrlGen Group Plc</p>
+    </div>
+  )
+}
+
+export default Footer
